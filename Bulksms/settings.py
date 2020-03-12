d92b78194bf0b9b77c1c8ec6162efcf308d99111
root <root@mail.icon-rim.com>
2020-03-12 13:42:33 +0000

    sms server

--- Bulksms/settings.py
+++ Bulksms/settings.py
@@ -205,7 +205,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'Bulksms/static')
